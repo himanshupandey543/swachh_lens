68110d67c6a762c3ab12ec12d3e95a2804399777
Himanshu Pandey <hp184020@gmail.com>
2026-08-23 09:43:31 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -10,8 +10,7 @@
  * edit the PRODUCTION_API_URL below after deploying the backend.
  * ===================================================================== */
 (function () {
-  var PRODUCTION_API_URL = 'swachhlens-production.up.railway.app/api';
-
+  var PRODUCTION_API_URL = 'https://swachhlens-production.up.railway.app/api';
   window.SW_CONFIG = window.SW_CONFIG || {};
   window.SW_CONFIG.APP_NAME = 'SwachhLens';
   window.SW_CONFIG.TICKER = 'Smart Waste Management';
